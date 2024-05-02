2fc6e88e8f4d5cd528bee4c2cc0831fcc054fc41
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-01 18:02:20 -0600

    agregando componentes necesarios

---
+++ src/app/components/feed-back/feed-back.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-back',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed-back.component.html',
+  styleUrl: './feed-back.component.css'
+})
+export class FeedBackComponent {
+
+}
