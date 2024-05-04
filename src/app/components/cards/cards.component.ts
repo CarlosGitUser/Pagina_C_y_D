111ed8ad3b70b0692f11d357d83dac2dbcbca85a
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-03 18:30:17 -0600

    obtener datos desde la API en card

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-cards',
@@ -9,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class CardsComponent {
 
+  constructor(public producstService: ProductsService){}
+
+  datos:any = [];
+  ruta  = "../../../assets/img/";
+  ngOnInit(){
+    console.log("Cargando ngOnInit...");
+    this.obtenerDatos();
+  }
+
+  obtenerDatos():void{
+    console.log("Entrando a funcion obtenerDatos");
+    this.producstService.retornar().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err) => {console.log(err)}
+    });
+  }
+
+  successRequest(data:any):void{
+    console.log("Datos recibidos del API", data.products);
+    this.datos = data.products;
+  }
+
+  crearRuta(tipo:string, img:string):string{
+    console.log(this.ruta+tipo+"/"+img);
+    return this.ruta+tipo+"/"+img;
+  }
 }
