c0ec0ec679bbe87cc4a6be4cadc2d45c57461a6e
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-03 18:49:49 -0600

    arreglando problema con app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent,BreadcrumbComponent,CardsComponent],
+  imports: [RouterOutlet, HeaderComponent,BreadcrumbComponent,CardsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
