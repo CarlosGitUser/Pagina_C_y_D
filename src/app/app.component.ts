cdea9691f06f53553d44ff10d50d2931edd6fd24
Jazmin Martinez <jazminmartinez55371@gmail.com>
2024-05-03 18:10:22 -0600

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { CardsComponent } from './components/cards/cards.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent,BreadcrumbComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent,BreadcrumbComponent,CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
