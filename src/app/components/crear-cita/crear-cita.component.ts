94a06ddee334adb3249152cdfc95b68659d949aa
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-04 16:21:36 -0600

    agregegando logica de validacion

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-crear-cita',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent, FormsModule],
   templateUrl: './crear-cita.component.html',
   styleUrl: './crear-cita.component.css'
 })
 export class CrearCitaComponent{
-  
+
+  cita = {
+    nombre : '',
+    telefono : 0,
+    email : '',
+    fecha : null,
+    hora : '',
+    lugar : '',
+    canthuespedes : 0
+  }
+
+  validacion():void{
+    console.log(this.cita);
+  }
+
+  validarFecha(fecha:Date):boolean{
+    
+  return true;
+  }
 }
