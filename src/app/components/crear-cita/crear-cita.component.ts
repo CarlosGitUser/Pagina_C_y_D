93af165fec94f5bf63ccc40235789d355252835c
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-05 11:43:34 -0600

    validacion de fecha y de nombre

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-crear-cita',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent, FormsModule],
+  imports: [HeaderComponent,FooterComponent, FormsModule, CommonModule],
   templateUrl: './crear-cita.component.html',
   styleUrl: './crear-cita.component.css'
 })
@@ -23,12 +24,46 @@ export class CrearCitaComponent{
     canthuespedes : 0
   }
 
-  validacion():void{
-    console.log(this.cita);
+  validacion():boolean{
+    // if(this.cita.nombre != '' && this.cita.telefono != 0 && this.cita.fecha != null){
+    //   console.log(this.cita.fecha);
+    //   return false;
+    // }
+    // return true;
+    return true;
   }
 
-  validarFecha(fecha:Date):boolean{
-    
-  return true;
+  validarFecha(fechaString:string):boolean{
+    if(fechaString != null ){
+      let auxDate = fechaString.split('-');
+      console.log(auxDate);
+      let fecha = new Date(parseInt(auxDate[0]), parseInt(auxDate[1])-1, parseInt(auxDate[2]));
+      console.log("Fecha recibida: " + fecha);
+      let fechaHoy = new Date();
+      if(fecha >= fechaHoy){
+        console.log("La fecha recibida es menor a la actual");
+        return false
+      } else return true;
+    }
+
+  return false;
   }
+
+  crearCita():void{
+    //console.log("Cita: "+this.cita);
+  }
+
+  validarNombre(nombre:string):boolean{
+    if(nombre != null && nombre.length >=6 )
+      return false;
+    return true;
+  }
+
+  validarHora(hora:string):boolean{
+    if(hora != null){
+      console.log("a");
+    }
+    return false;
+  }
+
 }
