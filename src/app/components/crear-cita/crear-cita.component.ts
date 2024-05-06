d1569fe49397e89da132752b9f796fe32c96a829
Jazmin Martinez <jazminmartinez55371@gmail.com>
2024-05-05 18:33:39 -0600

    tabla quitada de crearcita

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -3,14 +3,13 @@ import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { TableComponent } from '../table/table.component';
 import Swal from 'sweetalert2';
 
 
 @Component({
   selector: 'app-crear-cita',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent, FormsModule, CommonModule,TableComponent],
+  imports: [HeaderComponent,FooterComponent, FormsModule, CommonModule],
   templateUrl: './crear-cita.component.html',
   styleUrl: './crear-cita.component.css'
 })
