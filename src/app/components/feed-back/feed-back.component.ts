0cb4749c196ad640b79abeb89ad1b317690e20e3
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2024-05-06 00:55:22 -0600

    alertas de sweetalert para quejas y sugerencias

--- src/app/components/feed-back/feed-back.component.ts
+++ src/app/components/feed-back/feed-back.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-feed-back',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './feed-back.component.html',
   styleUrl: './feed-back.component.css'
 })
 export class FeedBackComponent {
+  enviarSugerencia(){
+    Swal.fire({
+      title: "!!!Gracias por tu comentario!!!",
+      text: "Tomaremos en cuenta tu opinion para mejorar nuestra pagina",
+      icon: "success"
+    });
+  }
 
+  enviarQueja(){
+    Swal.fire({
+      title: "Lamentamos la molestia",
+      text: "Tus comentarios nos ayudan a mejorar nuestra pagina y hacer la experiencia mas llevadera",
+      icon: "info"
+    });
+  }
 }
